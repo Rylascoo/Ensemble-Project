ee44485c6bd18f20e514a7d6a66838cad554dab0
Rylascoo <rylascoo@gmail.com>
2026-09-01 22:25:45 -0500

    Add fixture version regression tests

---
+++ tests/Ensemble.E0.Core.Tests/Fixture/FixtureVersionTests.cs
@@ -0,0 +1,31 @@
+using Ensemble.E0.Core.Fixture;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Ensemble.E0.Core.Tests.Fixture;
+
+[TestClass]
+public sealed class FixtureVersionTests
+{
+    [TestMethod]
+    public void CanonicalStableVersion_IsAccepted()
+    {
+        Assert.AreEqual("0.1.0", FixtureVersion.From("0.1.0").ToString());
+    }
+
+    [DataTestMethod]
+    [DataRow("01.0.0")]
+    [DataRow("1.0")]
+    [DataRow("1.0.0-beta")]
+    [DataRow("1.0.0+build")]
+    [DataRow("")]
+    public void NonCanonicalOrUnsupportedVersion_IsRejected(string value)
+    {
+        Assert.Throws<ArgumentException>(() => FixtureVersion.From(value));
+    }
+
+    [TestMethod]
+    public void DefaultVersion_RejectsStringification()
+    {
+        Assert.Throws<InvalidOperationException>(() => default(FixtureVersion).ToString());
+    }
+}
